bff9a396ec4c14b3a6a5331eb3a7d7e707922e87
boredgamesdev <23066086+boredgamesdev@users.noreply.github.com>
2025-08-18 07:32:24 -0400

    Update deploy_kali.sh

--- deploy_kali.sh
+++ deploy_kali.sh
@@ -47,7 +47,7 @@ install_packages() {
     printf "${GREEN}\nInstalling apt packages\n${NC}"
     apt-get -y install ca-certificates curl gnupg lsb-release seclists curl dnsrecon enum4linux feroxbuster gobuster \
     impacket-scripts nbtscan nikto nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g \
-    whatweb wkhtmltopdf python3-pip evil-winrm chromium jq tmux python3-venv python3-pip terminator zaproxy sliver rlwrap jython \
+    whatweb python3-pip evil-winrm chromium jq tmux python3-venv python3-pip terminator zaproxy sliver rlwrap jython \
     bloodhound xorg xrdp > /dev/null
 }
 
